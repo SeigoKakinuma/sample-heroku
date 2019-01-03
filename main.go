e5eefe3737c7ab6634ff834acb07463ad66bfb20
SeigoKakinuma <seigo.k-4253@hotmail.co.jp>
2019-01-03 09:09:22 +0900

    goファイル9

--- main.go
+++ main.go
@@ -1,34 +1,18 @@
 package main
- 
+
 import (
-	"fmt"
-	"log"
+	"github.com/zenazn/goji"
+	"github.com/zenazn/goji/web"
 	"net/http"
-	"os"
+	"text/template"
 )
- 
-func determineListenAddress() (string, error) {
-	port := os.Getenv("PORT")
-	if port == "" {
-		return "", fmt.Errorf("$PORT not set")
-	}
-	return ":" + port, nil
-}
- 
-func hello(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Hello World")
-}
- 
+
 func main() {
-	addr, err := determineListenAddress()
-	if err != nil {
-		log.Fatal(err)
-	}
- 
-	http.HandleFunc("/", hello)
-	log.Printf("Listening on %s...\n", addr)
- 
-	if err := http.ListenAndServe(addr, nil); err != nil {
-		panic(err)
-	}
+	goji.Get("/", IndexHandler)
+	goji.Serve()
+}
+
+func IndexHandler(c web.C, w http.ResponseWriter, r *http.Request) {
+	t := template.Must(template.ParseFiles("index.html"))
+	t.Execute(w, nil)
 }
